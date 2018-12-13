5876a3b50f251dd4e80fd123cb60eb52dcdb7c32
Huizhuo Gan <jokerandjoe@gmail.com>
2018-12-13 01:52:52 +0000

    add some more jqery in it

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
 /* global $ */
-
+    var name = $("#name").val();
 $(document).ready(function() {
     $("button").click(function() {
-        var name = $("#name").val();
-        var techPlacement = "TBD";
-        var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
+    var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
+
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var q4Result = $("#question4").val();
+        things1(q1Result,q2Result,q3Result,q4Result);
+        
     });
+
+
+
+function congrats(name,techPlacement){
+    $(".result").html("Congratluates to"+name+"Your dream job is "+techPlacement);
+}
+function things1(q1Result,q2Result,q3Result,q4Result){
+   var q1num=q1Result.split(" ").length;
+   var q3num=q3Result.split(" ").length;
+   var q4num=q4Result.split(" ").length;
+   var q2num=Number(q2Result)
+   var totalScore=q1num+q2num+q3num+q4num;
+   if(totalScore>10){
+       congrats(name,"Dva");
+   }
+   else if(totalScore>15){
+       congrats(name,"Genji");
+   }
+   else if(totalScore>30){
+       congrats(name,"McCree")
+   }
+   else if(totalScore>40){
+       
+   }
+   
+}
+
 
 });
